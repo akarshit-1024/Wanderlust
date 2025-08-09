0d086115d2c74a179799189381ee68b3e4df36ce
SDE Akarshit <akarshitmaurya8266@gmail.com>
2025-08-09 21:40:34 +0530

    move expressError file in utils folder

---
+++ utils/ExpressError.js
@@ -0,0 +1,9 @@
+class ExpressError extends Error{
+    constructor(status,message) {
+        super(message);
+        this.status = status;
+        this.name = "ExpressError";
+    };
+};
+
+module.exports=ExpressError;
