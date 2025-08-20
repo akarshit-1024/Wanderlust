57033cd0a558077bd99c0a421cda4afceea04de7
SDE Akarshit <akarshitmaurya8266@gmail.com>
2025-08-20 16:05:58 +0530

    move listing route And review routes. I use in place of these route App use Router method. And move some file to router/listing.js and router/review.js

--- app.js
+++ app.js
@@ -2,12 +2,12 @@ const express=require("express");
 const mongoose=require("mongoose");
 const path=require("path");
 const methodOverride=require("method-override");
-const ejsMate=require("ejs-mate");
-const Listing=require("./models/listing.js");
+const ejsMate=require("ejs-mate"); 
 const ExpressError=require("./utils/ExpressError.js");
-const wrapAsync= require("./utils/wrapAsync.js");
-const {listingSchema, reviewSchema}=require("./schema.js");
-const Review=require("./models/review.js");
+
+const listingRoutes=require("./routes/listing.js");
+const reviewRoutes=require("./routes/review.js");
+
 const app=express();
 
 app.engine("ejs",ejsMate);
@@ -16,6 +16,8 @@ app.set("views",path.join(__dirname,"views"));
 app.use(express.static(path.join(__dirname,"public")));
 app.use(methodOverride("_method"));
 app.use(express.urlencoded({extended:true}));
+
+
 
 //connect to MongoDB
 
@@ -30,115 +32,9 @@ main().then(()=>{
 async function main(){
     await mongoose.connect(MONGO_URL);
 } 
-//validate the request body against the schema
-//if validation fails, throw an ExpressError with status 400 and error message
-const ValidateListing=(req,res,next)=>{
-    const {error}=listingSchema.validate(req.body);
-    if(error){
-        const errorMessage=error.details.map((el)=>el.message).join(",");
-        throw new ExpressError(400,errorMessage);
-    }else{
-        next();
-    }
-};
-//for review validation
-const validateReview=(req,res,next)=>{
-    const {error}=reviewSchema.validate(req.body);
-    if(error){
-        const errorMessage=error.details.map((el)=>el.message).join(",");
-        throw new ExpressError(400,errorMessage);
-    }else{
-        next();
-    }
-};
 
-//show all the listings
-app.get("/listinges",wrapAsync(async (req,res)=>{
-    const listinges=await Listing.find();
-    res.render("./listings/index.ejs",{allDataOfListings:listinges});
-    
-}));
-//create a new listing
-app.get("/listinges/new",(req,res)=>{
-    res.render("./listings/new.ejs");
-});
-//save a new listing
-
-app.post("/listinges",ValidateListing,wrapAsync(async (req,res)=>{
-    const newListing=new Listing(req.body.listingObj);
-    await newListing.save();
-    res.redirect("/listinges");
-})
-);
-//open a individual listing
-app.get("/listinges/:id",wrapAsync(async (req,res)=>{
-    let {id}=req.params;
-    const findData=await Listing.findById(id).populate("reviews");
-    res.render("./listings/show.ejs",{listing:findData});
-})
-);
-
-// //edit a listing
-
-app.get("/listinges/:id/edit",wrapAsync(async (req,res)=>{
-    let {id}=req.params;
-    const findData=await Listing.findById(id);
-    res.render("./listings/edit.ejs",{data:findData});
-}));
-//update a listing
-
-app.put("/listinges/:id",ValidateListing,wrapAsync(async (req,res)=>{
-    let {id}=req.params;
-    await Listing.findByIdAndUpdate(id,{...req.body.listingObj});
-    res.redirect("/listinges");
-}));
-//delete a listing
-app.delete("/listinges/:id",wrapAsync(async (req,res)=>{
-    let {id}=req.params;
-    console.log("Deleting listing with ID:", id);
-    await Listing.findByIdAndDelete(id);
-    res.redirect("/listinges");
-}));
-
-//review a listing
-app.post("/listinges/:id/review",validateReview,wrapAsync(async (req,res)=>{
-    const { id } = req.params;
-    const listing = await Listing.findById(id);
-    const newReview = new Review(req.body.review);
-
-    await newReview.save();
-    listing.reviews.push(newReview); // push review's ObjectId
-    await listing.save();
-    
-    res.redirect(`/listinges/${id}`); // redirect to listing page
-}));
-
-//delete a review
-app.delete("/listinges/:id/reviews/:reviewId", wrapAsync(async (req, res) => {
-    console.log("Deleting review...");
-    let {id,reviewId}=req.params;
-    console.log(id,reviewId);
-    await Listing.findByIdAndUpdate(id,{$pull:{reviews:reviewId}});
-    await Review.findByIdAndDelete(reviewId);
-    res.redirect(`/listinges/${id}`);
-    
-}));
-
-// Utility route to clean orphaned review IDs from all listings
-app.get("/cleanup-reviews", wrapAsync(async (req, res) => {
-    const validReviewIds = (await Review.find({}, "_id")).map(r => r._id.toString());
-    const listings = await Listing.find();
-    let cleaned = 0;
-    for (const listing of listings) {
-        const originalLength = listing.reviews.length;
-        listing.reviews = listing.reviews.filter(id => validReviewIds.includes(id.toString()));
-        if (listing.reviews.length !== originalLength) {
-            await listing.save();
-            cleaned++;
-        }
-    }
-    res.send(`Cleaned orphaned review IDs from ${cleaned} listings.`);
-}));
+app.use("/listinges", listingRoutes); // Use the listing routes 
+app.use("/listinges/:id/reviews",reviewRoutes);
 
 //404 error handler
 app.all(/.*/,(req,res,next)=>{
