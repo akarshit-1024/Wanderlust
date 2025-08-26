b8018075cf6f662ec77e800fc0187f57eee0fa40
SDE Akarshit <akarshitmaurya8266@gmail.com>
2025-08-26 09:28:07 +0530

    add isowner ,validationListing middleware in this file

--- middleware.js
+++ middleware.js
@@ -1,3 +1,8 @@
+const { listingSchema } = require("./schema.js");
+const Listing = require("./models/listing.js");
+const ExpressError=require("./utils/ExpressError.js");
+
+
 module.exports.isLogIn=(req,res,next)=>{
     if (!req.isAuthenticated()) {
         req.session.redirectUrl=req.originalUrl;
@@ -5,10 +10,28 @@ module.exports.isLogIn=(req,res,next)=>{
         return res.redirect("/login");
     }
     next();
-}
+};
 module.exports.saveRedirect=(req,res,next)=>{
     if(req.session.redirectUrl){
         res.locals.redirectUrl=req.session.redirectUrl;
-        return next();
     }
+    next();
+};
+module.exports.isOwner=async(req,res,next)=>{
+    let {id}=req.params;
+    let listing =await Listing.findById(id);
+    if(!req.user || listing.owner.equals(req.user._id)){
+        req.flash("error","You don't have permission to do that!");
+        return res.redirect(`/listinges/${id}`);
+    }
+    next();
 }
+module.exports.ValidateListing = (req, res, next) => {
+    const { error } = listingSchema.validate(req.body);
+    if (error) {
+        const errorMessage = error.details.map((el) => el.message).join(",");
+        throw new ExpressError(400, errorMessage);
+    } else {
+        next();
+    }
+};
