08df1a7a0d41a4c5a0089e3a6a08912eedfa1ba8
SDE Akarshit <akarshitmaurya8266@gmail.com>
2025-08-27 19:57:24 +0530

    add isLogIn ,validateReview middleware in post route and add isLogIn ,isReviewAuthor middleware in delete route

--- routes/review.js
+++ routes/review.js
@@ -2,27 +2,16 @@ const Review=require("../models/review.js");
 const express = require('express');
 const Listing=require("../models/listing.js");
 const wrapAsync= require("../utils/wrapAsync.js");
-const ExpressError=require("../utils/ExpressError.js");
-const { reviewSchema}=require("../schema.js");
-const router = express.Router({mergeParams:true});
+const router = express.Router({mergeParams:true}); 
+const {isLogIn,validateReview,isReviewAuthor}=require("../middleware.js");
 
-//for review validation
-const validateReview=(req,res,next)=>{
-    const {error}=reviewSchema.validate(req.body);
-    if(error){
-        const errorMessage=error.details.map((el)=>el.message).join(",");
-        throw new ExpressError(400,errorMessage);
-    }else{
-        next();
-    }
-};
 
 //review a listing
-router.post("/",validateReview,wrapAsync(async (req,res)=>{
+router.post("/",isLogIn,validateReview,wrapAsync(async (req,res)=>{
     const { id } = req.params;
     const listing = await Listing.findById(id);
     const newReview = new Review(req.body.review);
-
+    newReview.author=req.user._id;
     await newReview.save();
     listing.reviews.push(newReview); // push review's ObjectId
     await listing.save();
@@ -30,10 +19,8 @@ router.post("/",validateReview,wrapAsync(async (req,res)=>{
 }));
 
 //delete a review
-router.delete("/:reviewId", wrapAsync(async (req, res) => {
-    console.log("Deleting review...");
-    let {id,reviewId}=req.params;
-    console.log(id,reviewId);
+router.delete("/:reviewId",isLogIn,isReviewAuthor, wrapAsync(async (req, res) => {
+    let {id,reviewId}=req.params;  
     await Listing.findByIdAndUpdate(id,{$pull:{reviews:reviewId}});
     await Review.findByIdAndDelete(reviewId);
     res.redirect(`/listinges/${id}`);
