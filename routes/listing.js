f874d13dbe5cd97acc68b1d28f65ee09adef3b40
SDE Akarshit <akarshitmaurya8266@gmail.com>
2025-08-29 17:05:00 +0530

    Require malter Package. And use upload Method of Malter And also require Cloudconfig.js .And add in post route of Create listing. uplod.single(listingObj[img])

--- routes/listing.js
+++ routes/listing.js
@@ -1,20 +1,26 @@
-const express = require('express'); 
+const express = require('express');
+const router = express.Router();
 const wrapAsync = require("../utils/wrapAsync.js");
 const { isLogIn, ValidateListing, isOwner } = require("../middleware.js");
 const contListing = require("../controllers/listings.js");
-const router = express.Router();
-
+const multer = require("multer");
+const { storage } = require("../cloudConfig.js");
+const upload = multer({ storage }); // configure multer to save files to 'uploads/' directory
+//const { storage } = require('../cloudinary');
 //index route
 router.route("/")
     .get(wrapAsync(contListing.index))                                 /*index route*/
-    .post(ValidateListing, wrapAsync(contListing.createNewListing));  /*create route*/
+    .post(isLogIn,
+        
+        upload.single('listingObj[image]'),
+        wrapAsync(contListing.createNewListing))  /*create route*/
 //create route
 router.get("/new", isLogIn, contListing.createNewListingForm);
 //save route
 router
 //open listing route
 router.route("/:id")
-    .get(wrapAsync(contListing.showListing))                                         /*show route*/      
+    .get(wrapAsync(contListing.showListing))                                         /*show route*/
     .put(isLogIn, isOwner, ValidateListing, wrapAsync(contListing.updateListing))   /*update route*/
     .delete(isLogIn, isOwner, wrapAsync(contListing.distroyListing));              /*destroy route*/
 //edit route
