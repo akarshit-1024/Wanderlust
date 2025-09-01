93b7b78de32d35004435a34cbe7142cf9a05ead1
SDE Akarshit <akarshitmaurya8266@gmail.com>
2025-09-01 22:15:01 +0530

    some changing in createNewListing related to maps and require axios

--- controllers/listings.js
+++ controllers/listings.js
@@ -1,27 +1,47 @@
 const Listing = require("../models/listing.js");
+const axios = require("axios");
 
-module.exports.index=async (req, res) => {
+
+module.exports.index = async (req, res) => {
     const listinges = await Listing.find();
     res.render("./listings/index.ejs", { allDataOfListings: listinges });
 
 };
 
-module.exports.createNewListingForm=(req, res) => {
+module.exports.createNewListingForm = (req, res) => {
     res.render("./listings/new.ejs");
 
 };
 
-module.exports.createNewListing=async (req, res) => {
+module.exports.createNewListing = async (req, res) => {
     const newListing = new Listing(req.body.listingObj);
-    let {path,filename}=req.file;
-    newListing.image={url:path,filename:filename};
+    const {location,country} = newListing;
+    const searchQuery = `${location}, ${country}`;
+    const url = `https://api.maptiler.com/geocoding/${encodeURIComponent(searchQuery)}.json?key=${process.env.MAP_API_KEY}`;
+    const response = await axios.get(url);
+    const feature = response.data.features[0];
+
+    if (!feature) {
+        req.flash("error", "Location not found. Please try again.");
+        return res.redirect("/listinges/new");
+    }
+
+    // ✅ save GeoJSON properly
+    newListing.geometry = {
+        type: "Point",
+        coordinates: feature.geometry.coordinates  // [lng, lat]
+    };
+
+    let { path, filename } = req.file;
+    newListing.image = { url: path, filename: filename };
     newListing.owner = req.user._id;
+
     await newListing.save();
     req.flash("success", "Successfully made a new listing!");
     res.redirect("/listinges");
 };
 
-module.exports.showListing=async (req, res) => {
+module.exports.showListing = async (req, res) => {
     let { id } = req.params;
     const findData = await Listing.findById(id).populate({ path: "reviews", populate: { path: "author" } }).populate("owner");
     if (!findData) {
@@ -30,28 +50,28 @@ module.exports.showListing=async (req, res) => {
     }
     res.render("./listings/show.ejs", { listing: findData });
 }
-module.exports.editROute=async (req, res) => {
+module.exports.editROute = async (req, res) => {
     let { id } = req.params;
     const data = await Listing.findById(id);
-    
-    let originalUrl=data.image.url;
-    let originalImageUrl=originalUrl.replace("/upload","/upload/w_300,h_200");
 
-    res.render("./listings/edit.ejs", { data,originalImageUrl });
+    let originalUrl = data.image.url;
+    let originalImageUrl = originalUrl.replace("/upload", "/upload/w_300,h_200");
+
+    res.render("./listings/edit.ejs", { data, originalImageUrl });
 };
-module.exports.updateListing=async (req, res) => {
+module.exports.updateListing = async (req, res) => {
     let { id } = req.params;
-    let listing=await Listing.findByIdAndUpdate(id, { ...req.body.listingObj });
-    
-    if(typeof req.file!=="undifined"){
-    let {path,filename}=req.file;
-    listing.image={url:path,filename:filename};
-    await listing.save();
+    let listing = await Listing.findByIdAndUpdate(id, { ...req.body.listingObj });
+
+    if (typeof req.file !== "undifined") {
+        let { path, filename } = req.file;
+        listing.image = { url: path, filename: filename };
+        await listing.save();
     }
     req.flash("success", "Listing updated sucessfully!");
     res.redirect(`/listinges/${id}`);
 };
-module.exports.distroyListing=async (req, res) => {
+module.exports.distroyListing = async (req, res) => {
     let { id } = req.params;
     console.log("Deleting listing with ID:", id);
     await Listing.findByIdAndDelete(id);
